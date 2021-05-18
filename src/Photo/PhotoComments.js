57285be9504dd7aa20a25eb47650902ac8c3757f
Peterson Pereira <petersonbozza7@gmail.com>
2021-05-17 23:22:39 -0300

    adicionada parte de comentarios

--- src/Photo/PhotoComments.js
+++ src/Photo/PhotoComments.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { UserContext } from '../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComment.module.css';
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsRef = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+  React.useEffect(() => {
+    commentsRef.current.scrollTop = commentsRef.current.scrollHeight;
+  }, [comments]);
+  return (
+    <>
+      <ul ref={commentsRef} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
