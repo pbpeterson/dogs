7666fff53a0686704d94bc55ea529fee06887b58
Peterson Pereira <petersonbozza7@gmail.com>
2021-05-24 02:13:08 -0300

    adicionado propTypes em Feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const [ModalPhoto, setModalPhoto] = React.useState(null);
@@ -48,4 +49,14 @@ const Feed = ({ user }) => {
   );
 };
 
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
+};
 export default Feed;
