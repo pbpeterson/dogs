88c98f01758780e6c7ba759574ea043d3fdc3bd5
Peterson Pereira <petersonbozza7@gmail.com>
2021-05-25 23:46:30 -0300

    adicionado section de resetar senha

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import UseForm from '../../Hooks/UseForm';
+import useFetch from '../../Hooks/UseFetch';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import Error from '../Helper/Error';
+import { PASSWORD_LOST } from '../../api';
 
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = UseForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'),
+      });
+      request(url, options);
+    }
+  }
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
