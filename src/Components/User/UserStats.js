d458b8f969633f8fa55b12890a0d8932deb99be3
Peterson Pereira <petersonbozza7@gmail.com>
2021-05-26 01:49:05 -0300

    Adicionado biblioteca Victory de graficos

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import UseFetch from '../../Hooks/UseFetch';
 import Head from '../Helper/Head';
+import { GET_STATS } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      UserStats
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, loading, request, error } = UseFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
